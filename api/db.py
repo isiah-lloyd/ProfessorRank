75cdf7f85483d8b29d1d0375132462ddaf6032f9
isiah_lloyd <robotdude101@gmail.com>
2018-02-11 00:16:59 -0500

    Idk a lots changed. the night is young

--- api/db.py
+++ api/db.py
@@ -1,15 +1,19 @@
 import psycopg2
-from os import environ
+import os
 class DBconnection():
-  DATABASE_NAME = os.environ['DB_NAME']
-  DATABASE_USER_NAME = os.environ['DB_USER_NAME']
-  DATABASE_PASSWORD = os.environ['DB_PASSWORD']
-  DATABASE_HOST = os.environ['DB_HOST']
-  DATABASE_PORT = os.environ['DB_PORT']
   def __init__(self):
+    DATABASE_NAME = os.environ['DB_NAME']
+    DATABASE_USER_NAME = os.environ['DB_USER_NAME']
+    DATABASE_PASSWORD = os.environ['DB_PASSWORD']
+    DATABASE_HOST = os.environ['DB_HOST']
+    DATABASE_PORT = os.environ['DB_PORT']
     self.db_connection = psycopg2.connect(f'dbname={DATABASE_NAME} user={DATABASE_USER_NAME} password={DATABASE_PASSWORD} host={DATABASE_HOST} port={DATABASE_PORT}')
-    self.cursor = db.db_connection.cursor()
+    self.cursor = self.db_connection.cursor()
+
+  def createProfessor(self, name, school_id, department):
+    self.cursor.execute("""INSERT INTO professors(name, school_id, department) VALUES (%s, %s, %s)""", (name, school_id, department))
 
   def __del__(self):
+    self.db_connection.commit()
     self.cursor.close()
     self.db_connection.close()
--- api/handler.py
+++ api/handler.py
@@ -1,7 +1,17 @@
-import json
+#Required code for serverless-python-requirements to zip dependencies
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+#####################################################################
 
+import json
+import db
 
 def createProfessor(event, context):
+    http_body = json.loads(event['body'])
+    db_connection = db.DBconnection()
+    db_connection.createProfessor(http_body['name'], http_body['school_id'], http_body['department'])
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
@@ -50,3 +60,5 @@ def getReview(event, context):
     }
 
     return response
+if __name__ == "__main__":
+    createProfessor('', '')
